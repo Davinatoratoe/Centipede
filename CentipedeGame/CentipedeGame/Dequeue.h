28bf46c1d1772520293626af878673874081e890
David F <david.flintoft@gmail.com>
2019-03-27 15:41:56 +1100

    Implemented basic Dequeue class

--- CentipedeGame/CentipedeGame/Dequeue.h
+++ CentipedeGame/CentipedeGame/Dequeue.h
@@ -35,33 +35,91 @@ public:
 	~Dequeue()
 	{
 		while (head != nullptr)
-			PopBack();
+			PopFront();
 	}
 
 	void PushFront(const T& value)
 	{
-
+		if (head == nullptr)
+		{
+			head = new Node<T>(value, nullptr);
+			tail = head;
+		}
+		else
+		{
+			Node<T>* newNode = new Node<T>(value, head);
+			newNode->next = head;
+			head = newNode;
+		}
+		++size;
 	}
 
 	void PopFront()
 	{
-
+		if (head == nullptr)
+			return;
+		else if (head == tail)
+		{
+			delete head;
+			head = nullptr;
+			tail = nullptr;
+		}
+		else
+		{
+			Node<T>* oldNode = head;
+			head = head->next;
+			delete oldNose;
+		}
+		--size;
 	}
 
 	void PushBack(const T& value)
 	{
-
+		if (tail == nullptr)
+		{
+			tail = new Node<T>(value, nullptr);
+			head = tail;
+		}
+		else
+		{
+			Node<T>* newNode = new Node<T>(value, nullptr);
+			tail->next = newNode;
+			tail = newNode;
+		}
+		++size;
 	}
 
 	void PopBack()
 	{
-
+		if (tail == nullptr)
+			return;
+		else if (tail == head)
+		{
+			delete tail;
+			tail = nullptr;
+			head = nullptr;
+		}
+		else
+		{
+			Node<T>* node = head;
+			while (true)
+			{
+				if (node->next->next == nullptr)
+				{
+					delete node->next;
+					tail = node;
+					break;
+				}
+				node = node->next;
+			}
+		}
+		--size;
 	}
 
 	void Clear()
 	{
 		while (head != nullptr)
-			PopBack();
+			PopFront();
 		size = 0;
 	}
 
