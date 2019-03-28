0b1661f6af4a3770bbffa03d0964111b1dc5fda7
David F <david.flintoft@gmail.com>
2019-03-28 11:30:24 +1100

    Dynamic List working and tested

--- CentipedeGame/CentipedeGame/DynamicList.h
+++ CentipedeGame/CentipedeGame/DynamicList.h
@@ -44,7 +44,7 @@ public:
 	void Reserve(unsigned int amount)
 	{
 		T* newData = new T[capacity + amount];
-		memcpy(newData, data, capacity);
+		memcpy(newData, data, sizeof(T) * capacity);
 		delete data;
 		data = newData;
 		capacity += amount;
@@ -86,6 +86,20 @@ public:
 		size = other.size;
 		data = new T[capacity];
 		memcpy(data, other.data, sizeof(T) * capacity);
+		return *this;
+	}
+
+	friend ostream& operator<< (ostream& os, const List<T>& list)
+	{
+		os << "[";
+		for (unsigned int i = 0; i < list.Size(); ++i)
+		{
+			if (i != 0)
+				os << ", ";
+			os << list[i];
+		}
+		os << "]";
+		return os;
 	}
 
 	T& operator[] (unsigned int index) const
@@ -94,10 +108,12 @@ public:
 			return data[index];
 	}
 
-	void Display() const
+	void PrintDetails() const
 	{
 		cout << "Size: " << size << "   ";
+		cout << "Capacity: " << capacity << "   ";
 		for (unsigned int i = 0; i < size; ++i)
 			cout << data[i] << " ";
+		cout << endl;
 	}
 };
--- CentipedeGame/CentipedeGame/main.cpp
+++ CentipedeGame/CentipedeGame/main.cpp
@@ -2,25 +2,40 @@
 #include <iostream>
 #include "DynamicList.h"
 
+void TestDynamicList()
+{
+	cout << "--- Testing Dynamic List" << endl;
+
+	List<int> list;						//Default constructor
+	list.Push(30);						//Push
+	list.Push(110);
+	list.Push(220);
+	list.Pop();							//Pop
+	list.Reserve(5);					//Add more capacity
+	list.PrintDetails();				//Print out details (for testing)
+
+	List<int> list2(2);					//Overloaded constructor
+	list2.Push(20);
+	list2.Push(50);
+	list2.Push(70);						//Increases the capacity
+	list2.PrintDetails();
+
+	List<int> list3(list);				//Copy constructor
+	list3.Push(200);
+	list3.PrintDetails();
+
+	list3 = list;						//= (assignment) operator
+	list3.Push(100);
+	list3.PrintDetails();
+
+	cout << "List: " << list << endl;	//<< operator
+
+	cout << "--- Finished testing Dynamic List" << endl;
+}
+
 int main() 
 {
-	cout << "Testing Dynamic List" << endl;
-	
-	List<int> list;
-	list.Push(20);
-	list.Push(100);
-	list.Push(50);
-	list.Pop();
-	list.Clear();
-	list.Push(30);
-	list.Push(110);
-	list.Reserve(10);
-
-	list.Display();
-
-	cout << endl << "Finished testing Dynamic List" << endl;
-
-
+	TestDynamicList();
 	return 0;
 
 	// allocation
